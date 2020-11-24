b5ae5f91969192a95a6625b8f55091b19ed4ed0f
Veronika Takács <roni9764@gmail.com>
2020-11-24 11:43:33 +0100

    Add Bookcontext and Book model classes

---
+++ Models/BookContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WombatLibrarianApi.Models
+{
+    public class BookContext : DbContext
+    {
+        public DbSet<Book> TodoItems { get; set; }
+
+        public BookContext(DbContextOptions<BookContext> options) : base(options)
+        {
+        }
+    }
+}
