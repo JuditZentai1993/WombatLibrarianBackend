272c4bbe595773bc851f4d30a17b4a7e836f9a9d
juditzentai1993 <juditzentai1993@gmail.com>
2020-11-25 16:01:52 +0100

    Change book ID type to string from int

--- Models/Book.cs
+++ Models/Book.cs
@@ -8,7 +8,7 @@ namespace WombatLibrarianApi.Models
 {
     public class Book
     {
-        public int Id { get; set; }
+        public string Id { get; set; }
         public string Author { get; set; }
         public string Title { get; set; }
         public string Thumbnail { get; set; }
