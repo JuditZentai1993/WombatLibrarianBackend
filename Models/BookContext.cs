63dc03080d877cd1701f7c98ffb24f048c93f066
Dusernajder <dusungulu.tibi\gmail.com>
2020-11-26 14:11:36 +0100

    parse response from google books api

--- Models/BookContext.cs
+++ Models/BookContext.cs
@@ -25,31 +25,46 @@ namespace WombatLibrarianApi.Models
             using (var client = new HttpClient())
             {
                 var uri = new Uri(url);
-            
+
                 var response = await client.GetAsync(uri);
 
                 string textResult = await response.Content.ReadAsStringAsync();
 
                 JObject bookSearch = JObject.Parse(textResult);
 
-                IList<JToken> results = bookSearch["items"].Children().ToList();
-                foreach (JToken result in results)
+                IList<JToken> tokens = bookSearch["items"].Children().ToList();
+                foreach (JToken token in tokens)
                 {
-                    string id = result["id"].ToString();
-                    JObject details = (JObject)result["volumeInfo"];
-
-                    Book searchResult = details.ToObject<Book>();
-                    searchResult.Id = id;
-                    BookItems.Add(searchResult);
+                    BookItems.Add(parseJsonToken(token));
                 }
                 SaveChanges();
             }
         }
 
         private void clearBookItems()
-        {    
+        {
             BookItems.RemoveRange(BookItems);
             SaveChanges();
+        }
+
+        private Book parseJsonToken(JToken jToken)
+        {
+
+            JObject volumeInfo = (JObject)jToken["volumeInfo"];
+        
+            Book book = new Book();
+            book.Id = jToken["id"]?.ToString();
+            book.Title = volumeInfo["title"]?.ToString();
+            book.Description = volumeInfo["description"]?.ToString();
+            book.PageCount = volumeInfo["pageCount"] == null ? 0 : int.Parse( volumeInfo["pageCount"].ToString());
+            book.Rating = volumeInfo["averageRating"] == null ? 0 : double.Parse( volumeInfo["averageRating"].ToString());
+            book.RatingCount = volumeInfo["ratingsCount"] == null ? 0 : double.Parse( volumeInfo["ratingsCount"].ToString());
+            book.Language = volumeInfo["language"]?.ToString();
+            // book.Categories = volumeInfo["categories"]?.ToString(); // TODO: plural
+            book.MaturityRating = volumeInfo["maturityRating"]?.ToString();
+            // book.Published = DateTime.Parse(volumeInfo["publishedDate"]?.ToString());
+            book.Publisher = volumeInfo["publisher"]?.ToString();
+            return book;
         }
     }
 }
