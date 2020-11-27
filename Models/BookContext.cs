c1fe9e65b76a849a07efa1a673fe46602cf27630
Veronika Takács <roni9764@gmail.com>
2020-11-27 11:30:45 +0100

    Add JsonHelper util class, refactor BookContext to use util class

--- Models/BookContext.cs
+++ Models/BookContext.cs
@@ -38,7 +38,7 @@ namespace WombatLibrarianApi.Models
                 IList<JToken> tokens = bookSearch["items"].Children().ToList();
                 foreach (JToken token in tokens)
                 {
-                    BookItems.Add(parseJsonToken(token));
+                    BookItems.Add(JsonHelper.parseJsonToken(token));
                 }
                 await SaveChangesAsync();
             }
@@ -50,35 +50,6 @@ namespace WombatLibrarianApi.Models
             SaveChanges();
         }
 
-        private Book parseJsonToken(JToken jToken)
-        {
-
-            JObject volumeInfo = (JObject)jToken["volumeInfo"];
         
-            Book book = new Book();
-            book.Id = jToken["id"]?.ToString();
-            book.Title = volumeInfo["title"]?.ToString();
-            book.Subtitle = volumeInfo["subtitle"]?.ToString();
-            book.Thumbnail = volumeInfo["imageLinks"]?["thumbnail"]?.ToString();
-            if (volumeInfo["authors"] != null)
-            {
-                book.Authors = JsonConvert.DeserializeObject<IEnumerable<string>>((volumeInfo["authors"]).ToString())
-                    .Select(a => new Author{Name = a}).ToList();
-            }
-            book.Description = volumeInfo["description"]?.ToString();
-            book.PageCount = volumeInfo["pageCount"] == null ? 0 : int.Parse( volumeInfo["pageCount"].ToString());
-            book.Rating = volumeInfo["averageRating"] == null ? 0 : double.Parse( volumeInfo["averageRating"].ToString());
-            book.RatingCount = volumeInfo["ratingsCount"] == null ? 0 : double.Parse( volumeInfo["ratingsCount"].ToString());
-            book.Language = volumeInfo["language"]?.ToString();
-            if (volumeInfo["categories"] != null)
-            {
-                book.Categories = JsonConvert.DeserializeObject<IEnumerable<string>>((volumeInfo["categories"]).ToString())
-                    .Select(c => new Category{Name = c}).ToList();
-            }
-            book.MaturityRating = volumeInfo["maturityRating"]?.ToString();
-            book.Published = volumeInfo["publishedDate"]?.ToString();
-            book.Publisher = volumeInfo["publisher"]?.ToString();
-            return book;
-        }
     }
 }
