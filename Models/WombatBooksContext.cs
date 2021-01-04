bb024e4fcf52e5226c6eba97646f459102ecd68b
Veronika Takács <roni9764@gmail.com>
2021-01-04 21:56:51 +0100

    Create WombatBooksContext to unite all seperate db contexts, add necessary dbsets

---
+++ Models/WombatBooksContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+using System;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace WombatLibrarianApi.Models
+{
+    public class WombatBooksContext : DbContext
+    {
+        public WombatBooksContext(DbContextOptions<WombatBooksContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Book> BookShelfItems { get; set; }
+        public DbSet<Book> WishListItems { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
