a278adc6dc65ca30197ba413bf2e1da58bda1534
Veronika Takács <roni9764@gmail.com>
2021-01-04 23:42:38 +0100

    Remove unnecessary usings

--- Models/Book.cs
+++ Models/Book.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
+﻿using System.Collections.Generic;
 using System.Text.Json;
-using System.Threading.Tasks;
 
 namespace WombatLibrarianApi.Models
 {
