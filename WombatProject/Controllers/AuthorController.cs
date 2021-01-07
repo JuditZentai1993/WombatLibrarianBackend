9b81dcc3bec5135ed871a3b21ddd7356e2335690
Dusernajder <dusungulu.tibi@gmail.com>
2021-01-07 10:32:29 +0100

    refactor hierarchy

---
+++ WombatProject/Controllers/AuthorController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WombatLibrarianApi.Models;
+using WombatLibrarianApi.Services;
+
+namespace WombatLibrarianApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthorController : ControllerBase
+    {
+        private readonly BookAPIService _apiService;
+
+        public AuthorController(BookAPIService service)
+        {
+            _apiService = service;
+        }
+
+        // GET: api/Author/<authorname>
+        [HttpGet("{author}")]
+        public async Task<ActionResult<IEnumerable<Book>>> GetAuthorBookItems(string author)
+        {
+            await _apiService.GetAuthorBooks(author);
+            return _apiService.AuthorBookItems;
+        }
+    }
+}
---
+++ WombatProject/Models/Book.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Text.Json;
+
+namespace WombatLibrarianApi.Models
+{
+    public class Book
+    {
+        public string Id { get; set; }
+        public List<Author> Authors { get; set; }
+        public string Title { get; set; }
+        public string Subtitle { get; set; }
+        public string Thumbnail { get; set; }
+        public string Description { get; set; }
+        public int PageCount { get; set; }
+        public double Rating { get; set; }
+        public double RatingCount { get; set; }
+        public string Language { get; set; }
+        public List<Category> Categories { get; set; }
+        public string MaturityRating { get; set; }
+        public string Published { get; set; }
+        public string Publisher { get; set; }
+    
+        public override string ToString() => JsonSerializer.Serialize<Book>(this);
+    }
+}
