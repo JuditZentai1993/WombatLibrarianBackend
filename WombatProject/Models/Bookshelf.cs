eae8a71d55f406d8e44a8c668828c1453186a531
Veronika Takács <roni9764@gmail.com>
2021-01-19 16:02:05 +0100

    Add Bookshelf model

---
+++ WombatProject/Models/Bookshelf.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WombatLibrarianApi.Models
+{
+    public class Bookshelf
+    {
+        public int Id { get; set; }
+        public string BookId { get; set; }
+
+        public Book book;
+    }
+}
