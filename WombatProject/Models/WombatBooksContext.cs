8639e6a10164c8dd48c3ba38d2e53b5d9dc5db11
Veronika Takács <roni9764@gmail.com>
2021-01-19 16:03:20 +0100

    Add Bookshelf and Wishlist DbSets

--- WombatProject/Models/WombatBooksContext.cs
+++ WombatProject/Models/WombatBooksContext.cs
@@ -12,10 +12,11 @@ namespace WombatLibrarianApi.Models
 
         private IConfiguration _configuration;
 
-        public DbSet<Book> BookShelfItems { get; set; }
-        public DbSet<Book> WishListItems { get; set; }
+        public DbSet<Book> Books { get; set; }
         public DbSet<Author> Authors { get; set; }
         public DbSet<Category> Categories { get; set; }
+        public DbSet<Bookshelf> Bookshelves { get; set; }
+        public DbSet<Wishlist> Wishlists { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) 
             => optionsBuilder.UseSqlServer(_configuration.GetConnectionString("databaseConnection"));
