dddab31b0127556356b12317ef825c26cd7c47be
Tibor Düsüngülü <dusungulu.tibi@gmail.com>
2021-01-19 11:19:50 +0100

    sql server config

--- WombatProject/Models/WombatBooksContext.cs
+++ WombatProject/Models/WombatBooksContext.cs
@@ -1,16 +1,23 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace WombatLibrarianApi.Models
 {
     public class WombatBooksContext : DbContext
     {
-        public WombatBooksContext(DbContextOptions<WombatBooksContext> options) : base(options)
+        public WombatBooksContext(DbContextOptions<WombatBooksContext> options, IConfiguration configuration) : base(options)
         {
+            _configuration = configuration;
         }
+
+        private IConfiguration _configuration;
 
         public DbSet<Book> BookShelfItems { get; set; }
         public DbSet<Book> WishListItems { get; set; }
         public DbSet<Author> Authors { get; set; }
         public DbSet<Category> Categories { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) 
+            => optionsBuilder.UseSqlServer(_configuration.GetConnectionString("databaseConnection"));
     }
 }
