65b661fd1264196d4d365781791aa813cc2c6712
juditzentai1993 <juditzentai1993@gmail.com>
2021-02-16 15:15:07 -0800

    Change return value of GetBooksFromBookshelfAsync and GetBooksFromWishlistAsync from IEnumerable<object> to Ienumerable<Book>

--- WombatProject/Data/IBookRepository.cs
+++ WombatProject/Data/IBookRepository.cs
@@ -6,11 +6,11 @@ namespace WombatLibrarianApi.Services
 {
     public interface IBookRepository
     {
-        Task<IEnumerable<object>> GetBooksFromBookshelfAsync();
+        Task<IEnumerable<Book>> GetBooksFromBookshelfAsync();
         Task<Bookshelf> GetBookshelfItemByIdAsync(int id);
         Task<Bookshelf> AddBookToBookshelfAsync(Book book);
         Task RemoveBookFromBookshelfByIdAsync(string bookid);
-        Task<IEnumerable<object>> GetBooksFromWishlistAsync();
+        Task<IEnumerable<Book>> GetBooksFromWishlistAsync();
         Task<Wishlist> GetWishlistItemByIdAsync(int id);
         Task<Wishlist> AddBookToWishlistAsync(Book book);
         Task RemoveBookFromWishlistByIdAsync(string bookid);
