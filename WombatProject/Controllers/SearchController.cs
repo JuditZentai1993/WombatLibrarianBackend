62f5ea6eb910bd7ef42eeedeef460d0f21bb7e80
juditzentai1993 <juditzentai1993@gmail.com>
2021-02-16 15:18:54 -0800

    Add _mapper property to use AutoMapper; return the mapped list instead of the raw json list from the google api

--- WombatProject/Controllers/SearchController.cs
+++ WombatProject/Controllers/SearchController.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Threading.Tasks;
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using WombatLibrarianApi.Models;
 using WombatLibrarianApi.Services;
@@ -11,17 +12,21 @@ namespace WombatLibrarianApi.Controllers
     public class SearchController : ControllerBase
     {
         private readonly IBookAPIService _apiService;
+        private readonly IMapper _mapper;
 
-        public SearchController(IBookAPIService service)
+        public SearchController(IBookAPIService service, IMapper mapper)
         {
             _apiService = service;
+            _mapper = mapper;
         }
 
         // GET: api/Search/<searchphrase>
         [HttpGet("{searchTerm}")]
-        public async Task<ActionResult<IEnumerable<BookItem>>> GetBookItems(string searchTerm)
+        public async Task<ActionResult<IEnumerable<Book>>> GetBookItems(string searchTerm)
         {
-            return Ok(await _apiService.GetSearchResultsAsync(searchTerm));
+            var result = await _apiService.GetSearchResultsAsync(searchTerm);
+            var mappedResult = _mapper.Map<IEnumerable<Book>>(result);
+            return Ok(mappedResult);
         }
     }
 }
